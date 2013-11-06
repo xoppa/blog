f383f23a412c98a9b6eb9988023dc926513a2da2
Xoppa <contact@xoppa.nl>
2013-11-06 18:58:03 +0100

    Fix uniform name

--- tutorials/assets/createshader/data/test.vertex.glsl
+++ tutorials/assets/createshader/data/test.vertex.glsl
@@ -3,11 +3,11 @@ attribute vec3 a_normal;
 attribute vec2 a_texCoord0;
  
 uniform mat4 u_worldTrans;
-uniform mat4 u_projTrans;
+uniform mat4 u_projViewTrans;
  
 varying vec2 v_texCoord0;
  
 void main() {
     v_texCoord0 = a_texCoord0;
-    gl_Position = u_projTrans * u_worldTrans * vec4(a_position, 1.0);
+    gl_Position = u_projViewTrans * u_worldTrans * vec4(a_position, 1.0);
 }
