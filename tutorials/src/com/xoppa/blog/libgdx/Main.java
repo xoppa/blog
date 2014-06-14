db9b07bf4618046d5b259779342663385a9e2a8b
Xoppa <contact@xoppa.com>
2014-06-14 17:49:32 +0200

    Remove GL10 and add bullet part 2: dynamics

--- tutorials/src/com/xoppa/blog/libgdx/Main.java
+++ tutorials/src/com/xoppa/blog/libgdx/Main.java
@@ -47,22 +47,21 @@ public class Main extends JFrame {
 	public static class AppDesc {
 		public Class<? extends ApplicationListener> clazz;
 		public String title;
-		public boolean useGL20;
+		
 		public int width;
 		public int height;
 		public String data;
 		
-		public AppDesc(String title, boolean useGL20, int width, int height, Class<? extends ApplicationListener> clazz, String data) {
+		public AppDesc(String title, int width, int height, Class<? extends ApplicationListener> clazz, String data) {
 			this.clazz = clazz;
 			this.title = title;
-			this.useGL20 = useGL20;
 			this.width = width;
 			this.height = height;
 			this.data = data;
 		}
 		
-		public AppDesc(String title, boolean useGL20, int width, int height, Class<? extends ApplicationListener> clazz) {
-			this(title, useGL20, width, height, clazz, null);
+		public AppDesc(String title, int width, int height, Class<? extends ApplicationListener> clazz) {
+			this(title, width, height, clazz, null);
 		}
 		
 		@Override
@@ -75,84 +74,93 @@ public class Main extends JFrame {
 		"Tutorials",
 		new Object[] {
 				"Basic 3D using libgdx",
-				new AppDesc("step 1: render a cube", false, 640, 480, com.xoppa.blog.libgdx.g3d.basic3d.step1.Basic3DTest.class),
-				new AppDesc("step 2: lights", false, 640, 480, com.xoppa.blog.libgdx.g3d.basic3d.step2.Basic3DTest.class),
-				new AppDesc("step 3: camera controller", false, 640, 480, com.xoppa.blog.libgdx.g3d.basic3d.step3.Basic3DTest.class)			
+				new AppDesc("step 1: render a cube", 640, 480, com.xoppa.blog.libgdx.g3d.basic3d.step1.Basic3DTest.class),
+				new AppDesc("step 2: lights", 640, 480, com.xoppa.blog.libgdx.g3d.basic3d.step2.Basic3DTest.class),
+				new AppDesc("step 3: camera controller", 640, 480, com.xoppa.blog.libgdx.g3d.basic3d.step3.Basic3DTest.class)			
 		},
 		new Object[] {
 				"Load models using libgdx",
-				new AppDesc("step 1: load a wavefrom model", false, 640, 480, com.xoppa.blog.libgdx.g3d.loadmodels.step1.LoadModelsTest.class, "loadmodels/data"),
-				new AppDesc("step 2: use assetmanager", false, 640, 480, com.xoppa.blog.libgdx.g3d.loadmodels.step2.LoadModelsTest.class, "loadmodels/data"),
-				new AppDesc("step 3: multiple instances", false, 640, 480, com.xoppa.blog.libgdx.g3d.loadmodels.step3.LoadModelsTest.class, "loadmodels/data"),
-				new AppDesc("step 4: use fbx-conv", false, 640, 480, com.xoppa.blog.libgdx.g3d.loadmodels.step4.LoadModelsTest.class, "loadmodels/data")
+				new AppDesc("step 1: load a wavefrom model", 640, 480, com.xoppa.blog.libgdx.g3d.loadmodels.step1.LoadModelsTest.class, "loadmodels/data"),
+				new AppDesc("step 2: use assetmanager", 640, 480, com.xoppa.blog.libgdx.g3d.loadmodels.step2.LoadModelsTest.class, "loadmodels/data"),
+				new AppDesc("step 3: multiple instances", 640, 480, com.xoppa.blog.libgdx.g3d.loadmodels.step3.LoadModelsTest.class, "loadmodels/data"),
+				new AppDesc("step 4: use fbx-conv", 640, 480, com.xoppa.blog.libgdx.g3d.loadmodels.step4.LoadModelsTest.class, "loadmodels/data")
 		},
 		new Object[] {
 				"Loading a scene using libgdx",
-				new AppDesc("step 1: coding a scene", false, 640, 480, com.xoppa.blog.libgdx.g3d.loadscene.step1.LoadSceneTest.class, "loadscene/data"),
-				new AppDesc("step 2: combining models", false, 640, 480, com.xoppa.blog.libgdx.g3d.loadscene.step2.LoadSceneTest.class, "loadscene/data"),
-				new AppDesc("step 3: loading a modeled scene", false, 640, 480, com.xoppa.blog.libgdx.g3d.loadscene.step3.LoadSceneTest.class, "loadscene/data")
+				new AppDesc("step 1: coding a scene", 640, 480, com.xoppa.blog.libgdx.g3d.loadscene.step1.LoadSceneTest.class, "loadscene/data"),
+				new AppDesc("step 2: combining models", 640, 480, com.xoppa.blog.libgdx.g3d.loadscene.step2.LoadSceneTest.class, "loadscene/data"),
+				new AppDesc("step 3: loading a modeled scene", 640, 480, com.xoppa.blog.libgdx.g3d.loadscene.step3.LoadSceneTest.class, "loadscene/data")
 		},
 		new Object[] {
 				"Behind the 3D scenes",
-				new AppDesc("step 1: base code", false, 640, 480, com.xoppa.blog.libgdx.g3d.behindscenes.step1.BehindTheScenesTest.class, "behindscenes/data"),
-				new AppDesc("step 2: using ModelLoader", false, 640, 480, com.xoppa.blog.libgdx.g3d.behindscenes.step2.BehindTheScenesTest.class, "behindscenes/data"),
-				new AppDesc("step 3: change material by NodePart", false, 640, 480, com.xoppa.blog.libgdx.g3d.behindscenes.step3.BehindTheScenesTest.class, "behindscenes/data"),
-				new AppDesc("step 4: change material by name", false, 640, 480, com.xoppa.blog.libgdx.g3d.behindscenes.step4.BehindTheScenesTest.class, "behindscenes/data"),
-				new AppDesc("step 5: change material per ModelInstance", false, 640, 480, com.xoppa.blog.libgdx.g3d.behindscenes.step5.BehindTheScenesTest.class, "behindscenes/data"),
-				new AppDesc("step 6: using a Renderable", false, 640, 480, com.xoppa.blog.libgdx.g3d.behindscenes.step6.BehindTheScenesTest.class, "behindscenes/data"),
-				new AppDesc("step 7: using a Shader", true, 640, 480, com.xoppa.blog.libgdx.g3d.behindscenes.step7.BehindTheScenesTest.class, "behindscenes/data")
+				new AppDesc("step 1: base code", 640, 480, com.xoppa.blog.libgdx.g3d.behindscenes.step1.BehindTheScenesTest.class, "behindscenes/data"),
+				new AppDesc("step 2: using ModelLoader", 640, 480, com.xoppa.blog.libgdx.g3d.behindscenes.step2.BehindTheScenesTest.class, "behindscenes/data"),
+				new AppDesc("step 3: change material by NodePart", 640, 480, com.xoppa.blog.libgdx.g3d.behindscenes.step3.BehindTheScenesTest.class, "behindscenes/data"),
+				new AppDesc("step 4: change material by name", 640, 480, com.xoppa.blog.libgdx.g3d.behindscenes.step4.BehindTheScenesTest.class, "behindscenes/data"),
+				new AppDesc("step 5: change material per ModelInstance", 640, 480, com.xoppa.blog.libgdx.g3d.behindscenes.step5.BehindTheScenesTest.class, "behindscenes/data"),
+				new AppDesc("step 6: using a Renderable", 640, 480, com.xoppa.blog.libgdx.g3d.behindscenes.step6.BehindTheScenesTest.class, "behindscenes/data"),
+				new AppDesc("step 7: using a Shader", 640, 480, com.xoppa.blog.libgdx.g3d.behindscenes.step7.BehindTheScenesTest.class, "behindscenes/data")
 		},
 		new Object[] {
 				"Creating a shader with libgdx",
-				new AppDesc("step 1: render a sphere", true, 640, 480, com.xoppa.blog.libgdx.g3d.createshader.step1.ShaderTest.class, "createshader/data"),
-				new AppDesc("step 2: render points", true, 640, 480, com.xoppa.blog.libgdx.g3d.createshader.step2.ShaderTest.class, "createshader/data"),
-				new AppDesc("step 3: customize default shader", true, 640, 480, com.xoppa.blog.libgdx.g3d.createshader.step3.ShaderTest.class, "createshader/data"),
-				new AppDesc("step 4: implement shader", true, 640, 480, com.xoppa.blog.libgdx.g3d.createshader.step4.ShaderTest.class, "createshader/data"),
-				new AppDesc("step 5: enable depth test", true, 640, 480, com.xoppa.blog.libgdx.g3d.createshader.step5.ShaderTest.class, "createshader/data"),
-				new AppDesc("step 6: cache uniform locations", true, 640, 480, com.xoppa.blog.libgdx.g3d.createshader.step6.ShaderTest.class, "createshader/data")
+				new AppDesc("step 1: render a sphere", 640, 480, com.xoppa.blog.libgdx.g3d.createshader.step1.ShaderTest.class, "createshader/data"),
+				new AppDesc("step 2: render points", 640, 480, com.xoppa.blog.libgdx.g3d.createshader.step2.ShaderTest.class, "createshader/data"),
+				new AppDesc("step 3: customize default shader", 640, 480, com.xoppa.blog.libgdx.g3d.createshader.step3.ShaderTest.class, "createshader/data"),
+				new AppDesc("step 4: implement shader", 640, 480, com.xoppa.blog.libgdx.g3d.createshader.step4.ShaderTest.class, "createshader/data"),
+				new AppDesc("step 5: enable depth test", 640, 480, com.xoppa.blog.libgdx.g3d.createshader.step5.ShaderTest.class, "createshader/data"),
+				new AppDesc("step 6: cache uniform locations", 640, 480, com.xoppa.blog.libgdx.g3d.createshader.step6.ShaderTest.class, "createshader/data")
 		},
 		new Object[] {
 				"Using materials with libgdx",
-				new AppDesc("step 1: using modelbatch", true, 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step1.MaterialTest.class, "usingmaterials/data"),
-				new AppDesc("step 2: add a uniform", true, 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step2.MaterialTest.class, "usingmaterials/data"),
-				new AppDesc("step 3: using userData", true, 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step3.MaterialTest.class, "usingmaterials/data"),
-				new AppDesc("step 4: using ColorAttribute", true, 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step4.MaterialTest.class, "usingmaterials/data"),
-				new AppDesc("step 5: implement canRender", true, 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step5.MaterialTest.class, "usingmaterials/data"),
-				new AppDesc("step 6: add another color", true, 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step6.MaterialTest.class, "usingmaterials/data"),
-				new AppDesc("step 7: use custom attribute", true, 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step7.MaterialTest.class, "usingmaterials/data"),
-				new AppDesc("step 8: update canRender", true, 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step8.MaterialTest.class, "usingmaterials/data"),
-				new AppDesc("step 9: create custom attribute", true, 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step9.MaterialTest.class, "usingmaterials/data")
+				new AppDesc("step 1: using modelbatch", 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step1.MaterialTest.class, "usingmaterials/data"),
+				new AppDesc("step 2: add a uniform", 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step2.MaterialTest.class, "usingmaterials/data"),
+				new AppDesc("step 3: using userData", 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step3.MaterialTest.class, "usingmaterials/data"),
+				new AppDesc("step 4: using ColorAttribute", 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step4.MaterialTest.class, "usingmaterials/data"),
+				new AppDesc("step 5: implement canRender", 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step5.MaterialTest.class, "usingmaterials/data"),
+				new AppDesc("step 6: add another color", 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step6.MaterialTest.class, "usingmaterials/data"),
+				new AppDesc("step 7: use custom attribute", 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step7.MaterialTest.class, "usingmaterials/data"),
+				new AppDesc("step 8: update canRender", 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step8.MaterialTest.class, "usingmaterials/data"),
+				new AppDesc("step 9: create custom attribute", 640, 480, com.xoppa.blog.libgdx.g3d.usingmaterials.step9.MaterialTest.class, "usingmaterials/data")
 		},
 		new Object[] {
 				"3D Frustum culling",
-				new AppDesc("step 1: no frustum culling", false, 640, 480, com.xoppa.blog.libgdx.g3d.frustumculling.step1.FrustumCullingTest.class, "loadscene/data"),
-				new AppDesc("step 2: position culling", false, 640, 480, com.xoppa.blog.libgdx.g3d.frustumculling.step2.FrustumCullingTest.class, "loadscene/data"),
-				new AppDesc("step 3: bounds culling", false, 640, 480, com.xoppa.blog.libgdx.g3d.frustumculling.step3.FrustumCullingTest.class, "loadscene/data"),
-				new AppDesc("step 4: sphere culling", false, 640, 480, com.xoppa.blog.libgdx.g3d.frustumculling.step4.FrustumCullingTest.class, "loadscene/data")
+				new AppDesc("step 1: no frustum culling", 640, 480, com.xoppa.blog.libgdx.g3d.frustumculling.step1.FrustumCullingTest.class, "loadscene/data"),
+				new AppDesc("step 2: position culling", 640, 480, com.xoppa.blog.libgdx.g3d.frustumculling.step2.FrustumCullingTest.class, "loadscene/data"),
+				new AppDesc("step 3: bounds culling", 640, 480, com.xoppa.blog.libgdx.g3d.frustumculling.step3.FrustumCullingTest.class, "loadscene/data"),
+				new AppDesc("step 4: sphere culling", 640, 480, com.xoppa.blog.libgdx.g3d.frustumculling.step4.FrustumCullingTest.class, "loadscene/data")
 		},
 		new Object[] {
 				"Ray picking",
-				new AppDesc("step 1: selecting objects", false, 640, 480, com.xoppa.blog.libgdx.g3d.raypicking.step1.RayPickingTest.class, "loadscene/data"),
-				new AppDesc("step 2: moving objects", false, 640, 480, com.xoppa.blog.libgdx.g3d.raypicking.step2.RayPickingTest.class, "loadscene/data"),
-				new AppDesc("step 3: preciser selecting objects", false, 640, 480, com.xoppa.blog.libgdx.g3d.raypicking.step3.RayPickingTest.class, "loadscene/data")
+				new AppDesc("step 1: selecting objects", 640, 480, com.xoppa.blog.libgdx.g3d.raypicking.step1.RayPickingTest.class, "loadscene/data"),
+				new AppDesc("step 2: moving objects", 640, 480, com.xoppa.blog.libgdx.g3d.raypicking.step2.RayPickingTest.class, "loadscene/data"),
+				new AppDesc("step 3: preciser selecting objects", 640, 480, com.xoppa.blog.libgdx.g3d.raypicking.step3.RayPickingTest.class, "loadscene/data")
 		},
 		new Object[] {
 			"Shapes",
-			new AppDesc("step 1: move code to GameObject", false, 640, 480, com.xoppa.blog.libgdx.g3d.shapes.step1.ShapeTest.class, "loadscene/data"),
-			new AppDesc("step 2: Sphere shape", false, 640, 480, com.xoppa.blog.libgdx.g3d.shapes.step2.ShapeTest.class, "loadscene/data"),
-			new AppDesc("step 3: Box shape", false, 640, 480, com.xoppa.blog.libgdx.g3d.shapes.step3.ShapeTest.class, "loadscene/data"),
-			new AppDesc("step 4: Disc shape", false, 640, 480, com.xoppa.blog.libgdx.g3d.shapes.step4.ShapeTest.class, "loadscene/data")
+			new AppDesc("step 1: move code to GameObject", 640, 480, com.xoppa.blog.libgdx.g3d.shapes.step1.ShapeTest.class, "loadscene/data"),
+			new AppDesc("step 2: Sphere shape", 640, 480, com.xoppa.blog.libgdx.g3d.shapes.step2.ShapeTest.class, "loadscene/data"),
+			new AppDesc("step 3: Box shape", 640, 480, com.xoppa.blog.libgdx.g3d.shapes.step3.ShapeTest.class, "loadscene/data"),
+			new AppDesc("step 4: Disc shape", 640, 480, com.xoppa.blog.libgdx.g3d.shapes.step4.ShapeTest.class, "loadscene/data")
 		},
 		new Object[] {
 			"Bullet: Collision detection",
-			new AppDesc("step 1: no collision detection", false, 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step1.BulletTest.class),
-			new AppDesc("step 2: using a collision algorithm", false, 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step2.BulletTest.class),
-			new AppDesc("step 3: using a collision dispatcher", false, 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step3.BulletTest.class),
-			new AppDesc("step 4: add more objects", false, 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step4.BulletTest.class),
-			new AppDesc("step 5: using a ContactListener", false, 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step5.BulletTest.class),
-			new AppDesc("step 6: optimize the callback method", false, 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step6.BulletTest.class),
-			new AppDesc("step 7: using a collision world", false, 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step7.BulletTest.class),
-			new AppDesc("step 8: using collision filtering", false, 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step8.BulletTest.class)
+			new AppDesc("step 1: no collision detection", 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step1.BulletTest.class),
+			new AppDesc("step 2: using a collision algorithm", 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step2.BulletTest.class),
+			new AppDesc("step 3: using a collision dispatcher", 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step3.BulletTest.class),
+			new AppDesc("step 4: add more objects", 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step4.BulletTest.class),
+			new AppDesc("step 5: using a ContactListener", 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step5.BulletTest.class),
+			new AppDesc("step 6: optimize the callback method", 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step6.BulletTest.class),
+			new AppDesc("step 7: using a collision world", 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step7.BulletTest.class),
+			new AppDesc("step 8: using collision filtering", 640, 480, com.xoppa.blog.libgdx.g3d.bullet.collision.step8.BulletTest.class)
+		},
+		new Object[] {
+			"Bullet: Dynamics",
+			new AppDesc("step 1: add physics properties", 640, 480, com.xoppa.blog.libgdx.g3d.bullet.dynamics.step1.BulletTest.class),
+			new AppDesc("step 2: add a dynamics world", 640, 480, com.xoppa.blog.libgdx.g3d.bullet.dynamics.step2.BulletTest.class),
+			new AppDesc("step 3: color objects on the ground", 640, 480, com.xoppa.blog.libgdx.g3d.bullet.dynamics.step3.BulletTest.class),
+			new AppDesc("step 4: add motion state", 640, 480, com.xoppa.blog.libgdx.g3d.bullet.dynamics.step4.BulletTest.class),
+			new AppDesc("step 5: contact callback filtering", 640, 480, com.xoppa.blog.libgdx.g3d.bullet.dynamics.step5.BulletTest.class),
+			new AppDesc("step 6: kinematic body", 640, 480, com.xoppa.blog.libgdx.g3d.bullet.dynamics.step6.BulletTest.class)
 		}
 	};
 	
@@ -172,7 +180,7 @@ public class Main extends JFrame {
 		if (currentTest != null)
 			container.remove(currentTest.getCanvas());
 		
-		currentTest = new LwjglAWTCanvas(listener, appDesc.useGL20);
+		currentTest = new LwjglAWTCanvas(listener);
 		currentTest.getCanvas().setSize(appDesc.width, appDesc.height);
 		container.add(currentTest.getCanvas(), BorderLayout.CENTER);
 		pack();
---
+++ tutorials/src/com/xoppa/blog/libgdx/g3d/bullet/dynamics/step3/BulletTest.java
@@ -0,0 +1,250 @@
+package com.xoppa.blog.libgdx.g3d.bullet.dynamics.step3;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.bullet.Bullet;
+import com.badlogic.gdx.physics.bullet.collision.ContactListener;
+import com.badlogic.gdx.physics.bullet.collision.btBoxShape;
+import com.badlogic.gdx.physics.bullet.collision.btBroadphaseInterface;
+import com.badlogic.gdx.physics.bullet.collision.btCapsuleShape;
+import com.badlogic.gdx.physics.bullet.collision.btCollisionConfiguration;
+import com.badlogic.gdx.physics.bullet.collision.btCollisionDispatcher;
+import com.badlogic.gdx.physics.bullet.collision.btCollisionObject;
+import com.badlogic.gdx.physics.bullet.collision.btCollisionShape;
+import com.badlogic.gdx.physics.bullet.collision.btConeShape;
+import com.badlogic.gdx.physics.bullet.collision.btCylinderShape;
+import com.badlogic.gdx.physics.bullet.collision.btDbvtBroadphase;
+import com.badlogic.gdx.physics.bullet.collision.btDefaultCollisionConfiguration;
+import com.badlogic.gdx.physics.bullet.collision.btDispatcher;
+import com.badlogic.gdx.physics.bullet.collision.btSphereShape;
+import com.badlogic.gdx.physics.bullet.dynamics.btConstraintSolver;
+import com.badlogic.gdx.physics.bullet.dynamics.btDiscreteDynamicsWorld;
+import com.badlogic.gdx.physics.bullet.dynamics.btDynamicsWorld;
+import com.badlogic.gdx.physics.bullet.dynamics.btRigidBody;
+import com.badlogic.gdx.physics.bullet.dynamics.btSequentialImpulseConstraintSolver;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ArrayMap;
+import com.badlogic.gdx.utils.Disposable;
+
+public class BulletTest implements ApplicationListener {
+	final static short GROUND_FLAG = 1<<8;
+	final static short OBJECT_FLAG = 1<<9;
+	final static short ALL_FLAG = -1;
+	
+	class MyContactListener extends ContactListener {
+		@Override
+		public boolean onContactAdded (int userValue0, int partId0, int index0, int userValue1, int partId1, int index1) {
+			if (userValue0 != 0)
+				((ColorAttribute)instances.get(userValue0).materials.get(0).get(ColorAttribute.Diffuse)).color.set(Color.WHITE);
+			if (userValue1 != 0)
+				((ColorAttribute)instances.get(userValue1).materials.get(0).get(ColorAttribute.Diffuse)).color.set(Color.WHITE);
+			return true;
+		}
+	}
+
+	static class GameObject extends ModelInstance implements Disposable {
+		public final btRigidBody body;
+
+		public GameObject (Model model, String node, btRigidBody.btRigidBodyConstructionInfo constructionInfo) {
+			super(model, node);
+			body = new btRigidBody(constructionInfo);
+		}
+
+		@Override
+		public void dispose () {
+			body.dispose();
+		}
+
+		static class Constructor implements Disposable {
+			public final Model model;
+			public final String node;
+			public final btCollisionShape shape;
+			public final btRigidBody.btRigidBodyConstructionInfo constructionInfo;
+			private static Vector3 localInertia = new Vector3();
+
+			public Constructor (Model model, String node, btCollisionShape shape, float mass) {
+				this.model = model;
+				this.node = node;
+				this.shape = shape;
+				if (mass > 0f)
+					shape.calculateLocalInertia(mass, localInertia);
+				else
+					localInertia.set(0, 0, 0);
+				this.constructionInfo = new btRigidBody.btRigidBodyConstructionInfo(mass, null, shape, localInertia);
+			}
+
+			public GameObject construct () {
+				return new GameObject(model, node, constructionInfo);
+			}
+
+			@Override
+			public void dispose () {
+				shape.dispose();
+				constructionInfo.dispose();
+			}
+		}
+	}
+
+	PerspectiveCamera cam;
+	CameraInputController camController;
+	ModelBatch modelBatch;
+	Environment environment;
+	Model model;
+	Array<GameObject> instances;
+	ArrayMap<String, GameObject.Constructor> constructors;
+	float spawnTimer;
+
+	btCollisionConfiguration collisionConfig;
+	btDispatcher dispatcher;
+	MyContactListener contactListener;
+	btBroadphaseInterface broadphase;
+   btDynamicsWorld dynamicsWorld;
+   btConstraintSolver constraintSolver;
+
+	@Override
+	public void create () {
+		Bullet.init();
+
+		modelBatch = new ModelBatch();
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.set(3f, 7f, 10f);
+		cam.lookAt(0, 4f, 0);
+		cam.near = 1f;
+		cam.far = 300f;
+		cam.update();
+
+		camController = new CameraInputController(cam);
+		Gdx.input.setInputProcessor(camController);
+
+		ModelBuilder mb = new ModelBuilder();
+		mb.begin();
+		mb.node().id = "ground";
+		mb.part("ground", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.RED)))
+			.box(5f, 1f, 5f);
+		mb.node().id = "sphere";
+		mb.part("sphere", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.GREEN)))
+			.sphere(1f, 1f, 1f, 10, 10);
+		mb.node().id = "box";
+		mb.part("box", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.BLUE)))
+			.box(1f, 1f, 1f);
+		mb.node().id = "cone";
+		mb.part("cone", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.YELLOW)))
+			.cone(1f, 2f, 1f, 10);
+		mb.node().id = "capsule";
+		mb.part("capsule", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.CYAN)))
+			.capsule(0.5f, 2f, 10);
+		mb.node().id = "cylinder";
+		mb.part("cylinder", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal,
+			new Material(ColorAttribute.createDiffuse(Color.MAGENTA))).cylinder(1f, 2f, 1f, 10);
+		model = mb.end();
+
+		constructors = new ArrayMap<String, GameObject.Constructor>(String.class, GameObject.Constructor.class);
+		constructors.put("ground", new GameObject.Constructor(model, "ground", new btBoxShape(new Vector3(2.5f, 0.5f, 2.5f)), 0f));
+		constructors.put("sphere", new GameObject.Constructor(model, "sphere", new btSphereShape(0.5f), 1f));
+		constructors.put("box", new GameObject.Constructor(model, "box", new btBoxShape(new Vector3(0.5f, 0.5f, 0.5f)), 1f));
+		constructors.put("cone", new GameObject.Constructor(model, "cone", new btConeShape(0.5f, 2f), 1f));
+		constructors.put("capsule", new GameObject.Constructor(model, "capsule", new btCapsuleShape(.5f, 1f), 1f));
+		constructors.put("cylinder", new GameObject.Constructor(model, "cylinder", new btCylinderShape(new Vector3(.5f, 1f, .5f)), 1f));
+
+      collisionConfig = new btDefaultCollisionConfiguration();
+      dispatcher = new btCollisionDispatcher(collisionConfig);
+      broadphase = new btDbvtBroadphase();
+      constraintSolver = new btSequentialImpulseConstraintSolver();
+      dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, constraintSolver, collisionConfig);
+      dynamicsWorld.setGravity(new Vector3(0, -10f, 0));
+      contactListener = new MyContactListener();
+       
+      instances = new Array<GameObject>();
+      GameObject object = constructors.get("ground").construct();
+      instances.add(object);
+      dynamicsWorld.addRigidBody(object.body, GROUND_FLAG, ALL_FLAG);
+	}
+
+	public void spawn () {
+		GameObject obj = constructors.values[1 + MathUtils.random(constructors.size - 2)].construct();
+		obj.transform.setFromEulerAngles(MathUtils.random(360f), MathUtils.random(360f), MathUtils.random(360f));
+		obj.transform.trn(MathUtils.random(-2.5f, 2.5f), 9f, MathUtils.random(-2.5f, 2.5f));
+		obj.body.setWorldTransform(obj.transform);
+		obj.body.setUserValue(instances.size);
+		obj.body.setCollisionFlags(obj.body.getCollisionFlags() | btCollisionObject.CollisionFlags.CF_CUSTOM_MATERIAL_CALLBACK);
+		instances.add(obj);
+		dynamicsWorld.addRigidBody(obj.body, OBJECT_FLAG, GROUND_FLAG);
+	}
+
+	@Override
+	public void render () {
+		final float delta = Math.min(1f / 30f, Gdx.graphics.getDeltaTime());
+
+		dynamicsWorld.stepSimulation(delta, 5, 1f/60f);
+
+		for (GameObject obj : instances)
+			obj.body.getWorldTransform(obj.transform);
+
+		if ((spawnTimer -= delta) < 0) {
+			spawn();
+			spawnTimer = 1.5f;
+		}
+
+		camController.update();
+
+		Gdx.gl.glClearColor(0.3f, 0.3f, 0.3f, 1.f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		modelBatch.begin(cam);
+		modelBatch.render(instances, environment);
+		modelBatch.end();
+	}
+
+	@Override
+	public void dispose () {
+		for (GameObject obj : instances)
+			obj.dispose();
+		instances.clear();
+
+		for (GameObject.Constructor ctor : constructors.values())
+			ctor.dispose();
+		constructors.clear();
+
+		dynamicsWorld.dispose();
+      constraintSolver.dispose();
+		broadphase.dispose();
+		dispatcher.dispose();
+		collisionConfig.dispose();
+
+		contactListener.dispose();
+
+		modelBatch.dispose();
+		model.dispose();
+	}
+
+	@Override
+	public void pause () {
+	}
+
+	@Override
+	public void resume () {
+	}
+
+	@Override
+	public void resize (int width, int height) {
+	}
+}
